ebebef5b6e8a4932f9c8e7334e07c7036c4b8a08
yap006 <tom.severas@student.uib.no>
2018-03-07 20:14:18 +0100

    Fixed minor bugs

--- src/inf101/v18/rogue101/objects/Player.java
+++ src/inf101/v18/rogue101/objects/Player.java
@@ -1,44 +1,63 @@
 package inf101.v18.rogue101.objects;
 
+import inf101.v18.gfx.gfxmode.ITurtle;
 import inf101.v18.grid.GridDirection;
 import inf101.v18.rogue101.game.IGame;
-import inf101.v18.rogue101.objects.IItem;
-import inf101.v18.rogue101.objects.IPlayer;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
 
 public class Player implements IPlayer {
-
-
-
+    private int hp = getMaxHealth();
+    @Override
+    public boolean draw(ITurtle painter, double w, double h) {
+        painter.save();
+        painter.turn(75);
+        double size = ((double) hp + getMaxHealth()) / (2.0 * getMaxHealth());
+        double carrotLength = size * h * .6;
+        double carrotWidth = size * h * .4;
+        painter.jump(-carrotLength / 6);
+        painter.shape().ellipse().width(carrotLength).height(carrotWidth).fillPaint(Color.RED).fill();
+        painter.jump(carrotLength / 2);
+        painter.setInk(Color.BLUE);
+        for (int i = -1; i < 2; i++) {
+            painter.save();
+            painter.turn(20 * i);
+            painter.draw(carrotLength / 3);
+            painter.restore();
+        }
+        painter.restore();
+        return true;
+    }
     @Override
     public void keyPressed(IGame game, KeyCode key) {
         if (key == KeyCode.LEFT) {
             tryToMove(game, GridDirection.WEST);
         }
-        else if (key == KeyCode.RIGHT){
+        if (key == KeyCode.RIGHT){
             tryToMove(game, GridDirection.EAST);
         }
-        else if (key == KeyCode.UP){
+        if (key == KeyCode.UP){
             tryToMove(game, GridDirection.NORTH);
         }
-        else if (key == KeyCode.DOWN){
+       if (key == KeyCode.DOWN){
             tryToMove(game, GridDirection.SOUTH);
         }
+        showStatus(game);
 
     }
 
-
-    public void tryToMove(IGame game, GridDirection dir){
-        if (game.canGo(dir)){
+    public void tryToMove(IGame game, GridDirection dir) {
+        if (game.canGo(dir)) {
             game.move(dir);
+            return;
         }
         else{
-            game.displayMessage("Wanker!");
+            game.displayMessage("Wanker");
         }
-        showStatus(game);
     }
 
-    @Override
+
+        @Override
     public int getAttack() {
         return 0;
     }
@@ -50,7 +69,7 @@ public class Player implements IPlayer {
 
     @Override
     public int getCurrentHealth() {
-        return 0;
+        return 100;
     }
 
     @Override
@@ -60,7 +79,7 @@ public class Player implements IPlayer {
 
     @Override
     public int getMaxHealth() {
-        return 0;
+        return 100;
     }
 
     @Override
@@ -75,7 +94,7 @@ public class Player implements IPlayer {
 
     @Override
     public String getSymbol() {
-        return null;
+        return "R";
     }
 
     @Override
@@ -86,4 +105,5 @@ public class Player implements IPlayer {
     public void showStatus(IGame game){
         game.displayStatus("NAme: " + getName() + ", Score: " + getCurrentHealth());
     }
+
 }
