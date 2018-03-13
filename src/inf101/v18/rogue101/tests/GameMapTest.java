71c6acc06ec40875babaa03f8f6ee8ea99d76cbb
yap006 <tom.severas@student.uib.no>
2018-03-13 22:38:21 +0100

    Tests for visibility

--- src/inf101/v18/rogue101/tests/GameMapTest.java
+++ src/inf101/v18/rogue101/tests/GameMapTest.java
@@ -12,25 +12,75 @@ import org.junit.jupiter.api.Test;
 import inf101.v18.grid.ILocation;
 import inf101.v18.rogue101.map.GameMap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class GameMapTest {
 
-	@Test
-	void testSortedAdd() {
-		GameMap gameMap = new GameMap(20, 20);
-		ILocation location = gameMap.getLocation(10, 10);
-		gameMap.add(location, new Carrot());
-		gameMap.add(location, new Rabbit());
-		gameMap.add(location, new Player());
+    @Test
+    void testSortedAdd() {
+        GameMap gameMap = new GameMap(20, 20);
+        ILocation location = gameMap.getLocation(10, 10);
+        gameMap.add(location, new Carrot());
+        gameMap.add(location, new Rabbit());
+        gameMap.add(location, new Player());
 
-		List<IItem> list = gameMap.getAll(location);
-		if(list.get(0).getSize() < list.get(1).getSize()) {
-			fail("Not yet implemented");
-		}
-		else if(list.get(1).getSize()<list.get(2).getSize()){
-			fail("Not yet implemented1");
-		}
-	}
+        List<IItem> list = gameMap.getAll(location);
+        if (list.get(0).getSize() < list.get(1).getSize()) {
+            fail("Not yet implemented");
+        } else if (list.get(1).getSize() < list.get(2).getSize()) {
+            fail("Not yet implemented1");
+        }
+    }
+
+
+    @Test
+    void visibleTest() {
+        GameMap map = new GameMap(20, 20);
+        ILocation loc = map.getLocation(10, 10);
+        List<ILocation> lst = map.getNeighbourhood(loc, 1);
+        for (int i = 0; i < lst.size(); i++) {
+            if (loc.gridDistanceTo(lst.get(i)) > 1) {
+                fail("Wrong distance");
+            }
+        }
+        if (lst.size() != 8) {
+            fail("You done fukced up" + lst.size());
+        }
+
+    }
+
+    @Test
+    void cornerTest() {
+        GameMap map = new GameMap(20, 20);
+        ILocation loc = map.getLocation(1, 1);
+        List<ILocation> lst = map.getNeighbourhood(loc, 2);
+        for (int i = 0; i < lst.size(); i++) {
+            if (loc.gridDistanceTo(lst.get(i)) > 2) {
+                fail("Wrong distance");
+            }
+        }
+        if (lst.size() != 15) {
+            fail("This number aint right" + lst.size());
+        }
+
+    }
+
+
+    @Test
+    void edgeTest(){
+        GameMap map = new GameMap(20, 20);
+        ILocation loc = map.getLocation(0, 10);
+        List<ILocation> lst = map.getNeighbourhood(loc, 2);
+        for (int i = 0; i < lst.size(); i++) {
+            if (loc.gridDistanceTo(lst.get(i)) > 2) {
+                fail("Wrong distance");
+            }
+        }
+        if (lst.size() != 14) {
+            fail("This number aint right" + lst.size());
+        }
+
+    }
 
 }
